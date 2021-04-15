0856ed631e987797cf86707e99ba3896c53bae9a
KristineLacampuenga <kristinelacampuenga@gmail.com>
2021-04-15 20:54:32 +0800

    Student Vioaltion Records Management System

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentViolationRecordsManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
